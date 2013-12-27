6c53bb83f5f7be07c1a97d0abafee5b24e828c69
jreidy <jreidy@stanford.edu>
2013-12-26 17:47:18 -0700

    creating repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MatchingTool::Application.config.secret_key_base = 'bb2084c2ababf95178a34578702d16ccb037d68df19d4938cd654bd6b0e876c2297eeff63d0726cc0ddf39503490916f499ebbf9f97d917a53f6daa8662ce6a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MatchingTool::Application.config.session_store :cookie_store, key: '_MatchingTool_session'
